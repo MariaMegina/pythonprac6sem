1fb25c892806c22c981f5e08060cc1b773280298
Maria <meginam16@gmail.com>
2024-03-25 14:30:17 +0300

    classwork

---
+++ 20240325/0/httpsrv.py
@@ -0,0 +1,9 @@
+import socket
+import sys
+from http.server import test, SimpleHTTPRequestHandler
+
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = sys.argv[1] if len(sys.argv) > 1 else "8000"
+print(HOST)
+test(SimpleHTTPRequestHandler, port=int(PORT))
+
